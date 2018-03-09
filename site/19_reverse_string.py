8e4519dd0f8159f2ecd71a06361165ea8aca4dec
luge2015 <1525397054@qq.com>
2018-03-09 19:27:20 +0800

    20

---
+++ site/19_reverse_string.py
@@ -0,0 +1,15 @@
+# -*-encoding:UTF-8 -*-
+
+#reverse string
+# input "hello"
+# output "olleh"
+
+def reverse_string():
+    a = "hello"
+    b = ""
+    for i in a[::-1]:
+        b+=i
+    print b
+
+if __name__=="__main__":
+    reverse_string()
---
+++ site/20_fizz_buzz.py
@@ -0,0 +1,34 @@
+# -*-encoding:UTF-8 -*-
+
+#outputs the string representation of numbers from 1 to n.
+# 3的倍数 用Fizz替代，5的倍数用Buzz替代
+
+
+def Fizz(n):
+    """
+
+    :param n:
+    :return:
+    """
+    a = []
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 != 0:
+            i = "Fizz"
+        elif i % 5 == 0 and i % 3 != 0:
+            i = "Buzz"
+        elif i% 15 == 0:
+            i = "FizzBuzz"
+
+        a.append(i)
+
+    return a
+
+
+def fizzBuzz(n):
+    return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+
+if __name__=="__main__":
+    n = 35
+    # print Fizz(n)
+
+    # print fizzBuzz(n)
