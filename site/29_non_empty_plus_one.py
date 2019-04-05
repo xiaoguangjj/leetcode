c69d5e1ffe15a7350fb8a01b28807f3a4c0926b7
jingxiaoguang <jingxiaoguang@socialcommer.com>
2019-04-05 15:49:57 +0800

    app

---
+++ site/29_non_empty_plus_one.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+"""
+加一
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+示例 1:
+
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+"""
+
+
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)):
+            print(i, ~i)
+            if digits[~i] < 9:
+                print(~i)
+                digits[~i] += 1
+                print(digits[~i])
+                return digits
+            digits[~i] = 0
+        return [1] + [0] * len(digits)
+
+
+if __name__ == '__main__':
+    a = [9,9,9,9]
+    print(Solution().plusOne(a))
---
+++ site/31_indices_sum_target.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+"""
+两数之和
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) < 2:
+            return
+        for i in range(0, len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+if __name__=='__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    print(Solution().twoSum(nums, target))
