0c2ce4329a4f06d1fd62955fa464a0dc24f9916d
jingxiaoguang <jingxiaoguang@socialcommer.com>
2019-04-15 10:57:55 +0800

    app

---
+++ site/34_reverse_string.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+"""
+反转字符串
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+
+
+
+示例 1：
+
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        for i, j in zip(range(0, len(s) // 2), range(len(s) - 1, -1, -1)):
+            s[i], s[j] = s[j], s[i]
+        return s
+
+
+if __name__ == '__main__':
+    s = ["h", "e", "l", "l", "o"]
+    print(Solution().reverseString(s))
