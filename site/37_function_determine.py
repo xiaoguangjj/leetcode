d97438779247d20e1646f08daf40c4aa6c137b89
jingxiaoguang <jingxiaoguang@socialcommer.com>
2019-04-16 15:38:47 +0800

    app

---
+++ site/37_function_determine.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+
+"""
+有效的字母异位词
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的一个字母异位词。
+"""
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        def splitstr(x):
+            y = []
+            for i in range(len(x)):
+                y.append(x[i])
+            return y
+
+        s = splitstr(s)
+        t = splitstr(t)
+        s.sort()
+        t.sort()
+        if s == t:
+            return True
+        return False
+
+
+if __name__=='__main__':
+    s = "anagram"
+    t = "nagaram"
+    # s = "rat"
+    # t = "car"
+    print(Solution().isAnagram(s, t))
