aa7486a89050db2c19af1a270f62d4eab4c043c5
jingxiaoguang <jingxiaoguang@socialcommer.com>
2019-04-18 19:39:49 +0800

    app

--- site/42_longest_common_string.py
+++ site/42_longest_common_string.py
@@ -9,26 +9,6 @@
 
 class Solution:
     def longestCommonPrefix(self, strs):
-        if len(strs) == 1:
-            return strs[0]
-        if len(strs) == 0:
-            return ""
-        min_len = min([ len(i) for i in strs])
-        if min_len == 0:
-            return ""
-        result = []
-        str_list = [list(i) for i in strs]
-        for i in range(min_len):
-            for j in range(len(strs)-1):
-                if str_list[j][i] != str_list[j+1][i]:
-                    return ''.join(result)
-                print(str_list[j][i])
-                result.append(str_list[j][i])
-                # if(min_len == i+1):
-                #     return ''.join(result)
-        return result
-
-    def longest(self,strs):
         if not strs:
             return ""
         if len(strs) == 1:
@@ -46,5 +26,4 @@ class Solution:
 if __name__ == '__main__':
     strs = ["flower", "flow", "flight"]
     # a = ["dog","racecar","car"]
-    # print(Solution().longestCommonPrefix(strs))
-    print(Solution().longest(strs))
+    print(Solution().longestCommonPrefix(strs))
