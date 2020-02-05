ad294ed1322e90472cdde8ccc65d30a6e8894e03
Spock <gaozanjun@Spock.local>
2020-02-05 22:39:46 +0800

    app

---
+++ site/71_isValid.py
@@ -0,0 +1,59 @@
+"""
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+"""
+算法原理：
+若遇到左括号入栈，遇到右括号时将对应栈顶括号出栈，若对应词典中是一对则true，否则为false
+栈 stack 为空： 此时 stack.pop() 操作会报错；因此，我们采用一个取巧方法，给 stack 赋初值 ?
+"""
+
+class Solution:
+    def isValid(self, s):
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic:
+                stack.append(c)
+            elif dic[stack.pop()] != c:
+                return False
+        return len(stack) == 1
+
+if __name__=="__main__":
+    # s = "()"
+    s ="(]"
+    a = Solution()
+    print(a.isValid(s))
