8b9f17f2ba55221b205d1ffd4e75213fc86b61a9
Spock <gaozanjun@Spock.local>
2020-02-14 18:51:42 +0800

    76

---
+++ site/75_findNumberIn2DArray.py
@@ -0,0 +1,68 @@
+"""
+在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+ 
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+ 
+
+限制：
+
+0 <= n <= 1000
+
+0 <= m <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+"""
+解题思路
+整个二维数组是有顺序的，所以可以按照从左到右，从上到下的顺序，以此增加。
+我们从左下角开始遍历，当该值小于 target 值时，向右搜索；大于 target 值时，向上搜索。如果找到 target 则返回 True，否则返回 False。
+
+"""
+
+class Solution:
+    def findNumberIn2DArray(self, matrix, target):
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        row, col = m - 1, 0
+        while row >= 0 and col < n:
+            if matrix[row][col] < target:
+                col += 1
+            elif matrix[row][col] > target:
+                row -= 1
+            else:
+                return True
+        return False
+
+
+if __name__=="__main__":
+    a = [
+        [1,   4,  7, 11, 15],
+        [2,   5,  8, 12, 19],
+        [3,   6,  9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]
+    # target = 5
+    target = 20
+    s = Solution()
+    print(s.findNumberIn2DArray(a, target))
---
+++ site/76_replaceSpace.py
@@ -0,0 +1,29 @@
+"""
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+ 
+
+示例 1：
+
+输入：s = "We are happy."
+输出："We%20are%20happy."
+ 
+
+限制：
+
+0 <= s 的长度 <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution:
+    def replaceSpace(self, s):
+        return s.replace(' ','%20')
+
+
+if __name__=="__main__":
+    s = "We are happy."
+    so = Solution()
+    print(so.replaceSpace(s))
