1bc189af3577f6a6ef9a98289e19be2f86c1549e
Spock <gaozanjun@Spock.local>
2020-03-12 21:06:31 +0800

    app

---
+++ site/81_movingCount.py
@@ -0,0 +1,43 @@
+"""
+机器人的运动范围:
+
+地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+
+ 
+
+示例 1：
+
+输入：m = 2, n = 3, k = 1
+输出：3
+示例 1：
+
+输入：m = 3, n = 1, k = 0
+输出：1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+class Solution:
+    def movingCount(self, m, n, k):
+        queue, visited, = [(0, 0, 0, 0)], set()
+        while queue:
+            i, j, si, sj = queue.pop(0)
+            if not 0 <= i < m or not 0 <= j < n or k < si + sj or (i, j) in visited: continue
+            visited.add((i, j))
+            queue.append((i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj))
+            queue.append((i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8))
+        return len(visited)
+
+
+if __name__=="__main__":
+    m = 2
+    n = 3
+    k = 1
+    s = Solution()
+    print(s.movingCount(m, n, k))
+
+
