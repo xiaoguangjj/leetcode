87f49e3833219c1f2a562208aac5769adc9b77a4
Spock <gaozanjun@Spock.local>
2020-06-30 16:10:49 +0800

    89

--- site/89_longestPalindrome.py
+++ site/89_longestPalindrome.py
@@ -13,12 +13,13 @@
 输出: "bb"
 """
 
+
 class Solution:
     def longestPalindrome(self, s):
         n = len(s)
         dp = [[False] * n for _ in range(n)]
         ans = ""
-        # 枚举子串的长度l + 1
+        # 枚举子串的长度 l+1
         for l in range(n):
             # 枚举子串的起始位置 i,这样可以通过 j=i+1 得到子串的结束位置
             for i in range(n):
@@ -30,7 +31,7 @@ class Solution:
                 elif l == 1:
                     dp[i][j] = (s[i] == s[j])
                 else:
-                    dp[i][j] = (dp[i + 1][j - 1]) and s[i] == s[j]
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
                 if dp[i][j] and l + 1 > len(ans):
                     ans = s[i:j+1]
         return ans
