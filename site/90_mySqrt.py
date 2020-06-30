4a4b42988ef139c3ea77c734064c108ca6853193
Spock <gaozanjun@Spock.local>
2020-06-30 17:59:21 +0800

    90

---
+++ site/90_mySqrt.py
@@ -0,0 +1,39 @@
+"""
+69. x 的平方根
+实现 int sqrt(int x) 函数。
+
+计算并返回 x 的平方根，其中 x 是非负整数。
+
+由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+示例 1:
+
+输入: 4
+输出: 2
+示例 2:
+
+输入: 8
+输出: 2
+说明: 8 的平方根是 2.82842...,
+     由于返回类型是整数，小数部分将被舍去。
+"""
+
+"""
+解法：我们将根号下x写成幂的形式，x的1/2次方，再使用自然对数e进行换底，即可得到
+"""
+
+import math
+
+
+class Solution:
+    def mySqrt(self, x):
+        if x == 0:
+            return 0
+        ans = int(math.exp(0.5 * math.log(x)))
+        return ans + 1 if (ans + 1) ** 2 <= x else ans
+
+
+if __name__=="__main__":
+    number = 8
+    s = Solution()
+    print(s.mySqrt(number))
