339a6845bd61cdf0451635d9c10ee76425c01479
Your Name <you@example.com>
2020-07-30 17:40:57 +0800

    print_directory_contents

---
+++ pycode/94_print_directory_contents.py
@@ -0,0 +1,27 @@
+"""
+写一个函数接收参数，返回该文件夹中的文件路径以及包含文件夹中文件的路径
+
+"""
+import os
+
+
+class Solution:
+    def print_directory_contents(self, sPath):
+        """
+        这个函数接受文件夹的名称作为输入参数，
+        返回该文件夹中文件的路径，
+        以及其包含文件夹中文件的路径。
+
+        """
+        for sChild in os.listdir(sPath):
+            sChildPath = os.path.join(sPath, sChild)
+            if os.path.isdir(sChildPath):
+                print_directory_contents(sChildPath)
+            else:
+                print(sChildPath)
+
+
+if __name__ == "__main__":
+    path = "/leetcode/pycode"
+    s = Solution()
+    s.print_directory_contents(path)
