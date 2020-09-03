0f9589cdb763b78d016ecc6b4c3a3d6b501f17e0
Your Name <you@example.com>
2020-09-03 23:46:54 +0800

    18

--- pycode/18_max_depth_btree.py
+++ pycode/18_max_depth_btree.py
@@ -1,8 +1,35 @@
 # -*-encoding:UTF-8 -*-
 
+"""
+104. 二叉树的最大深度
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+
+
+"""
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
 
 # 二叉树的最大深度
-class Max():
+class Max:
     def maxDepth(self, root):
         """
 
@@ -12,7 +39,7 @@ class Max():
         return 1 + max(map(self.maxDepth, (root.left, root.right))) if root else 0
 
 
-class MaxDepth():
+class MaxDepth:
     def maxDepth(self, root):
         """
 
@@ -24,3 +51,25 @@ class MaxDepth():
         leftChildHeight=self.maxDepth(root.lef)
         rightChidHeight=self.maxDepth(root.right)
         return max(leftChildHeight, rightChidHeight) + 1
+
+
+if __name__=="__main__":
+    """
+                2
+               / \
+              1   3
+                   \
+                    4
+                   / \
+                  6   5
+        """
+    Tree = TreeNode(2)
+    Tree.left = TreeNode(1)
+    aa = Tree.right = TreeNode(3)
+    bb = aa.right = TreeNode(4)
+    cc = TreeNode(5)
+    bb.right = cc
+    dd = TreeNode(6)
+    bb.left = dd
+    s = Max()
+    print(s.maxDepth(Tree))
