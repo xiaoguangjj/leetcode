b2cc112cda197aa5d622f47f8c6d2af8a8a69e76
Your Name <you@example.com>
2021-08-13 20:46:36 +0800

    generateParenthesis

--- Strings/22.generateParenthesis.py
+++ Strings/22.generateParenthesis.py
@@ -28,9 +28,25 @@
 
 会用到的方法：递归
 """
+
+
 class Solution:
     def generateParenthesis(self, n):
-        pass
+        ans = []
+        def backtrack(S, left, right):
+            if len(S) == 2 * n:
+                ans.append(''.join(S))
+                return
+            if left < n:
+                S.append('(')
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(')')
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack([], 0, 0)
+        return ans
 
 
 if __name__ == "__main__":
