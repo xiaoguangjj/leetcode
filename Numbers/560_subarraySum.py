2b52efeeb35ce3ad153a5b8bb7d2a309abd4bf31
Your Name <you@example.com>
2024-03-13 23:28:50 +0800

    和为 K 的子数组

---
+++ Numbers/560_subarraySum.py
@@ -0,0 +1,27 @@
+"""
+560. 和为 K 的子数组
+给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+
+子数组是数组中元素的连续非空序列。
+示例 1：
+
+输入：nums = [1,1,1], k = 2
+输出：2
+示例 2：
+
+输入：nums = [1,2,3], k = 3
+输出：2
+"""
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            sum = 0
+            for j in range(i, len(nums)):
+                sum += nums[j]
+                if sum == k:
+                    count += 1
+        return count
+
