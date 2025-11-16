fab639c60d59d4a163bf807db948715e48e2f35d
Your Name <you@example.com>
2025-11-16 18:21:01 +0800

    121.买卖股票的最佳时机

--- pycode/57_maximum_profit.py
+++ pycode/57_maximum_profit.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 """
-买卖股票的最佳时机
+121.买卖股票的最佳时机
 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
 
 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
@@ -36,6 +36,16 @@ class Solution:
             min_price = min(min_price, i)
             max_profile = max(max_profile, i - min_price)
         return max_profile
+
+    def maxprofit1(self, prices):
+        min_price = float("inf")
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+        return max_profit
 
 
 if __name__ == '__main__':
